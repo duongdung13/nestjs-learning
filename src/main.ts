c5be7fe95c4adcfa70f59a1d1ea2e46261b80754
Duong Dung <dungdcth@gmail.com>
2023-08-06 12:28:31 +0700

    Validation data

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe());
   const PORT = configService.get<string>('PORT');
   await app.listen(PORT, () => {
     console.log(`App listen port ${PORT}`);
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,20 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
 export class CreateUserDto {
+  @IsEmail(
+    {},
+    {
+      message: 'Email khong dung dinh dang',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email khong duoc de trong',
+  })
   email: string;
+
+  @IsNotEmpty()
   password: string;
+
   name: string;
+
   address: string;
 }
